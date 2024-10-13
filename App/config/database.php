de027ab0370c9ddaad5bd183423ca74dcfbf7e6b
marksoberano <127574744+Mackyato@users.noreply.github.com>
2024-10-13 23:03:14 +0800

    Second Commit

--- App/config/database.php
+++ App/config/database.php
@@ -120,18 +120,18 @@ return [
         ],
 
         'default' => [
-            'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
-            'password' => env('AZURE_REDIS_PASSWORD', null),
-            'port' => env('AZURE_REDIS_PORT', 6379),
-            'database' => env('AZURE_REDIS_DATABASE', 0),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_DB', 0),
         ],
 
         'cache' => [
             'scheme' => 'tls',
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_CACHE_DB', 1),
+            'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
+            'password' => env('AZURE_REDIS_PASSWORD', null),
+            'port' => env('AZURE_REDIS_PORT', 6379),
+            'database' => env('AZURE_REDIS_DATABASE', 1),
         ],
 
     ],
